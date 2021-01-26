1f4d799f89cd403305ab28cd7d9f9dec83e32faf
shubham kushwaha <shubhkush1234@gmail.com>
2021-01-26 19:10:43 +0530

    added super squad app

--- redux-collection/src/App.js
+++ redux-collection/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { Form, FormControl, Button } from 'react-bootstrap';
+import { render } from '@testing-library/react';
+import React from 'react';
+import AgeStats from './components/AgeStats';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      newDate: '',
+      birthday: '1995-11-13'
+    }
+  };
+
+  changeBirthday = () => {
+    console.log(this.state);
+    this.setState({ birthday: this.state.newDate });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h2>Enter your birthday:</h2>
+        <Form>
+          <FormControl type="date" onChange={(event) => this.setState({ newDate: event.target.value })}>
+          </FormControl><br />
+          {' '}
+          <Button onClick={() => this.changeBirthday()}>Submit</Button>
+          {/* if instead of writing () => this.changeBirthday() inside onClick,
+           if we give this.changeBirthday() alone, it will be executed even if the btn is not clicked */}
+          <AgeStats date={this.state.birthday || "enter some date dude"} />
+
+        </Form>
+        {' '}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ super-sqad/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h2>super squad</h2>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
