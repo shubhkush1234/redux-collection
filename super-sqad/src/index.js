e89ef569a0fe986d73de5db2033eab6d6cb7ce22
shubham kushwaha <shubhkush1234@gmail.com>
2021-01-28 09:49:49 +0530

    added reducer and action

--- super-sqad/src/index.js
+++ super-sqad/src/index.js
@@ -2,18 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
+import { createStore } from 'redux';
+import { addCharacterById } from './actions';
 
+let store = createStore(rootReducer);
+
+console.log("store.getState() ", store.getState());
+
+store.subscribe(() => console.log("store", store.getState()));
+
+// store.dispatch(addCharacterById(2));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider store={store}>
+      <App />
+    </Provider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- super-sqad/src/reducers/index.js
+++ super-sqad/src/reducers/index.js
@@ -1,7 +1,13 @@
 import characters_json from '../data/characters.json';
+import { ADD_CHARACTER } from '../actions';
 
 function characters(state= characters_json, action){
     switch(action.type){
+
+        case ADD_CHARACTER:
+            let Characters = state.filter(item => item.id !== action.id);
+            return Characters;
+
         default:
             return state;
     }
